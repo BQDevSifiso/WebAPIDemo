b67e701f69dcee3dcee24dcff1e9713773e24e4c
Sifiso Gumede <sifiso@bqsystems.co.za>
2024-05-31 00:19:48 +0200

    Handling Web Api Exceptions in Web App

--- WebApp/Controllers/ShirtsController.cs
+++ WebApp/Controllers/ShirtsController.cs
@@ -22,5 +22,93 @@ namespace WebApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateShirt(Shirt shirt)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var response = await webApiExecuter.InvokePost("shirts", shirt);
+                    if (response != null)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (WebApiException ex)
+                {
+                    HandleWebApiException(ex);
+                }
+               
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateShirt(int shirtId)
+        {
+            try
+            {
+                var shirt = await webApiExecuter.InvokeGet<Shirt>($"shirts/{shirtId}");
+                if (shirt != null)
+                {
+                    return View(shirt);
+                }
+            }
+            catch (WebApiException ex)
+            {
+                HandleWebApiException(ex);
+                return View();
+            }
+            
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateShirt(Shirt shirt)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await webApiExecuter.InvokePut($"shirts/{shirt.ShirtId}", shirt);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch(WebApiException ex)
+                {
+                    HandleWebApiException(ex);                    
+                }
+                
+            }
+            return View(shirt);
+        }
+
+        public async Task<IActionResult> DeleteShirt(int shirtId)
+        {
+            try
+            {
+                await webApiExecuter.InvokeDelete($"shirts/{shirtId}");
+                return RedirectToAction(nameof(Index));
+            }
+            catch(WebApiException ex)
+            {
+                HandleWebApiException(ex);
+                return View(nameof(Index), await webApiExecuter.InvokeGet<List<Shirt>>("shirts"));
+            }
+            
+        }
+
+        private void HandleWebApiException(WebApiException ex)
+        {
+            if (ex.ErrorResponse != null &&
+                        ex.ErrorResponse.Errors != null &&
+                        ex.ErrorResponse.Errors.Count > 0)
+            {
+                foreach (var error in ex.ErrorResponse.Errors)
+                {
+                    ModelState.AddModelError(error.Key, string.Join("; ", error.Value));
+                }
+            }
+        }
     }
 }
